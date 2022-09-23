71d61d8aefaede542cebbc61afa3488ac59ccce0
galimova-r <113368059+galimova-r@users.noreply.github.com>
2022-09-23 23:45:30 +0300

    Delete Main_Values.java

--- Main_Values.java
+++
@@ -1,9 +0,0 @@
-public class Main_Values {
-    public static void main(String[] args) {
-        Values values = new Values(new int[]{1,2,3}, new int[]{1,2,3});
-        for (int i = 0; i<6;i++){
-            int u = values.getValue();
-            System.out.println(u);
-        }
-    }
-}
