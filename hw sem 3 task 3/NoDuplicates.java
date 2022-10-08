eee215572e9f2558e2cec85c14a630fe9647db85
galimova-r <113368059+galimova-r@users.noreply.github.com>
2022-10-08 15:00:17 +0300

    Add files via upload

---
+++ hw sem 3 task 3/NoDuplicates.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class NoDuplicates {
+    public static void main(String[] args) {
+        ArrayList<Integer> ints = new ArrayList<>();
+        ints.add(1);
+        ints.add(2);
+        ints.add(1);
+        ints.add(3);
+        ints.add(2);
+        ints.add(3);
+        ints.add(10);
+        ArrayList<Integer> result = deleteDuplicates(ints);
+        System.out.println();
+    }
+
+    public static ArrayList deleteDuplicates(ArrayList list) {
+        ArrayList result = new ArrayList();
+        for (int i = 0; i < list.size(); i++) {
+            if (!result.contains(list.get(i))) {
+                result.add(list.get(i));
+            }
+        }
+        return result;
+    }
+}
